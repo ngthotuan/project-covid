4061ab35f6f1602583490859abfb54cd18ba8a5b
ngthotuan <ngthotuan@gmail.com>
2022-01-19 22:38:31 +0700

    update dashboard + some layout

--- src/controllers/site.controller.js
+++ src/controllers/site.controller.js
@@ -2,6 +2,11 @@ function index(req, res, next) {
     res.render('index', { title: 'Express' });
 }
 
+const dashboard = (req, res, next) => {
+    res.render('dashboard', { title: 'Dashboard' });
+};
+
 module.exports = {
     index,
+    dashboard,
 };
