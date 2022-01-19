2d9d25a1725606bcd0eab3acd2014427fcb41b93
ngthotuan <ngthotuan@gmail.com>
2022-01-19 17:00:24 +0700

    update success payment callback

--- src/controllers/apt.controller.js
+++ src/controllers/apt.controller.js
@@ -1,27 +1,40 @@
+const jsonwebtoken = require('jsonwebtoken');
+
 const { patientService, transactionHistoryService } = require('../services');
-const bcrypt = require('bcrypt');
 
 const callbackPayment = async (req, res, next) => {
-    const { dataCallback, amount, code } = req.query;
-    if (bcrypt.compareSync(process.env.CLIENT_SECRET, code)) {
-        const patient = await patientService.findByIdWithInclude(
-            dataCallback,
-            {},
-        );
+    const { token, success } = req.query;
+    if (success) {
+        try {
+            const { amount, dataCallback } = jsonwebtoken.verify(
+                token,
+                process.env.CLIENT_SECRET,
+            );
+            const patient = await patientService.findByIdWithInclude(
+                dataCallback,
+                {},
+            );
 
-        await patient.update({
-            debt: Math.abs(patient.debt - amount),
-        });
-        patient.save();
-        await transactionHistoryService.save({
-            amount,
-            created_date: Date.now(),
-            description: `Thang toán dư nợ`,
-            patient_id: patient.patient_id,
-        });
+            await patient.update({
+                debt: Math.abs(patient.debt - amount),
+            });
+            patient.save();
+            await transactionHistoryService.save({
+                amount,
+                created_date: Date.now(),
+                description: `Thanh toán dư nợ`,
+                patient_id: patient.patient_id,
+            });
+            req.flash('success_msg', 'Thanh toán thành công');
+        } catch (err) {
+            req.flash('error_msg', 'Đã có lỗi xảy ra');
+        }
+    } else {
+        req.flash('error_msg', 'Thanh toán thất bại');
     }
-    res.redirect('/details');
+    res.redirect('/users/payment-history');
 };
+
 module.exports = {
     callbackPayment,
 };
