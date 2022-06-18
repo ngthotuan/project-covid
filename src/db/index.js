89a8864794574028351554f8c56c3d57f82f7933
tuannt7 <tuannt7@vng.com.vn>
2022-06-18 17:11:20 +0700

    update db connect env

--- src/db/index.js
+++ src/db/index.js
@@ -3,12 +3,20 @@ require('dotenv').config();
 const models = require('../models');
 
 // Option 3: Passing parameters separately (other dialects)
-const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT } = process.env;
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-    host: DB_HOST,
-    dialect: DB_DIALECT /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
-    logging: false,
-});
+const { DB_URL, DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT } = process.env;
+const sequelize = DB_URL
+    ? new Sequelize(DB_URL, {
+          dialect: DB_DIALECT,
+          logging: false,
+          dialectOptions: { ssl: { rejectUnauthorized: false } },
+      })
+    : new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+          host: DB_HOST,
+          dialect:
+              DB_DIALECT /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+          logging: false,
+          dialectOptions: { ssl: { rejectUnauthorized: false } },
+      });
 
 async function connect() {
     try {
