3d35a471520c9e55be7edd35c127f4568a200f03
tuannt7 <tuannt7@vng.com.vn>
2022-06-23 16:03:59 +0700

    export unipin

--- src/controllers/export_log.controller.js
+++ src/controllers/export_log.controller.js
@@ -3,6 +3,7 @@ const util = require('../util');
 const moment = require('moment');
 const md5 = require('md5');
 const Op = require('sequelize').Op;
+const fs = require('fs');
 
 const { partnerAccountService, productMappingService } = require('../services');
 
@@ -180,6 +181,11 @@ const postExportPayerMax = async (req, res, next) => {
         console.log('PayerMax login success', token);
 
         let { products, startDate, endDate } = req.body;
+
+        if (typeof products === 'string') {
+            products = [products];
+        }
+
         if (!startDate || !endDate) {
             [startDate, endDate] = util.getDateRange();
         } else {
@@ -210,7 +216,7 @@ const postExportPayerMax = async (req, res, next) => {
                 startDate,
                 endDate,
             );
-            const currentProduct = `${pm.product_code_product.code} - ${pm.product_code_product.name}`;
+            const currentProduct = `${pm.product_code_product.code} - ${pm.product_code_product.name} (${pm.merchant_code} - ${pm.merchant_name})`;
             if (exportRes.code === '200') {
                 console.log(`Export data success for ${currentProduct}`);
                 successfulProduct.push(currentProduct);
@@ -236,15 +242,123 @@ const postExportPayerMax = async (req, res, next) => {
     }
 };
 
-const test = async (req, res, next) => {
-    res.render('export_logs/result', {
-        title: 'Result',
-        url: '123213',
-        message: 'success',
-        data: {
-            sdfsdf: 'sdfsdf',
-        },
-    });
+const getExportUnipin = async (req, res, next) => {
+    try {
+        const product_mappings =
+            await productMappingService.findAllByPartnerCode('unipin');
+        res.render('export_logs/unipin', {
+            title: 'Export Unipin',
+            url: 'https://backoffice.unipin.com',
+            product_mappings,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const exportDataUnipin = async (
+    unipin_backoffice_session,
+    merchantId,
+    exportFromDate,
+    exportToDate,
+) => {
+    try {
+        const response = await axios.get(
+            'https://backoffice.unipin.com/merchant-transactions/export',
+            {
+                params: {
+                    exportFromDate,
+                    exportToDate,
+                    exportStatus: 'SUCCESS',
+                    exportType: 0,
+                    exportMerchant: merchantId,
+                    exportCountry: 1,
+                    exportChannel: '',
+                    exportSearch: '',
+                    exportChannelName: true,
+                    export: 'csv',
+                },
+                headers: {
+                    Cookie: `unipin_backoffice_session=${unipin_backoffice_session}`,
+                },
+            },
+        );
+        console.log(response);
+        return response.data;
+    } catch (err) {
+        console.error('error: ', err.msg);
+        console.error(err);
+        return null;
+    }
+};
+
+const postExportUnipin = async (req, res, next) => {
+    try {
+        let { unipin_backoffice_session, products, startDate, endDate } =
+            req.body;
+
+        if (typeof products === 'string') {
+            products = [products];
+        }
+
+        if (!startDate || !endDate) {
+            [startDate, endDate] = util.getDateRange();
+            startDate = util.formatDate(startDate, 'MM/DD/YYYY');
+            endDate = util.formatDate(endDate, 'MM/DD/YYYY');
+        }
+
+        console.log('postExportPayerMax startDate', startDate);
+        console.log('postExportPayerMax endDate', endDate);
+
+        const condition = {
+            partner_code: 'unipin',
+            product_code: { [Op.in]: products },
+        };
+        console.log('postExportPayerMax condition', condition);
+        const product_mappings = await productMappingService.findAllByCondition(
+            condition,
+        );
+        const successfulProduct = [];
+        for (const pm of product_mappings) {
+            console.log(
+                'Call export: ',
+                JSON.stringify({
+                    unipin_backoffice_session,
+                    pm,
+                    startDate,
+                    endDate,
+                }),
+            );
+            const exportRes = await exportDataUnipin(
+                unipin_backoffice_session,
+                pm.merchant_code,
+                startDate,
+                endDate,
+            );
+            const currentProduct = `${pm.product_code_product.code} - ${pm.product_code_product.name} (${pm.merchant_code} - ${pm.merchant_name})`;
+            if (exportRes) {
+                console.log(`Export data success for ${currentProduct}`);
+                successfulProduct.push(currentProduct);
+                fs.writeFileSync(currentProduct + '.csv', exportRes);
+            } else {
+                console.error(
+                    `Export data failed for ${currentProduct}`,
+                    exportRes,
+                );
+            }
+        }
+        console.log('Successful product: ', successfulProduct.length);
+        console.log('Successful product: ', successfulProduct);
+
+        res.render('export_logs/result', {
+            title: 'Export Unipin',
+            // url: '',
+            message: `Export data success for ${successfulProduct.length} products`,
+            data: successfulProduct,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 module.exports = {
@@ -252,5 +366,6 @@ module.exports = {
     postExportAdyen,
     getExportPayerMax,
     postExportPayerMax,
-    test,
+    getExportUnipin,
+    postExportUnipin,
 };
--- src/routes/export_log.route.js
+++ src/routes/export_log.route.js
@@ -6,5 +6,6 @@ router.get('/adyen', exportLogController.getExportAdyen);
 router.post('/adyen', exportLogController.postExportAdyen);
 router.get('/payermax', exportLogController.getExportPayerMax);
 router.post('/payermax', exportLogController.postExportPayerMax);
-router.get('/test', exportLogController.test);
+router.get('/unipin', exportLogController.getExportUnipin);
+router.post('/unipin', exportLogController.postExportUnipin);
 module.exports = router;
